4a9913c2e22891c87db1752f769468456745ac52
nocksapp <nocksapp@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-09-03 18:27:15 +0000

    tagging version 0.0.3
    git-svn-id: https://plugins.svn.wordpress.org/nocks-nifty-for-woocommerce/tags/0.0.3@1237556 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ trunk/includes/ajax-price-estimate.php
@@ -0,0 +1,22 @@
+<?php
+function ajax_nocks_nifty_price_estimate()
+{
+    header('Content-Type: application/json');
+
+    $incomingCurrency = $_REQUEST['data']['incomingCurrency'];
+    $eurAmount = $_REQUEST['data']['amount'];
+
+    $nocks_nifty = new WC_Gateway_Nocks_Nifty();
+    $result = $nocks_nifty->calculate_price($incomingCurrency, $eurAmount);
+    $amount = $result['amount'];
+    $pair = explode('_', $result['pair']);
+    $amount = $nocks_nifty->nocks_nifty_check_price($pair, $amount);
+
+    echo json_encode(array(
+        'amount' => number_format($amount, 4, '.', '')
+    ));
+
+    die(); // Always die in functions echoing ajax content
+}
+add_action('wp_ajax_nopriv_ajax_nocks_nifty_price_estimate', 'ajax_nocks_nifty_price_estimate');
+add_action('wp_ajax_ajax_nocks_nifty_price_estimate', 'ajax_nocks_nifty_price_estimate');
